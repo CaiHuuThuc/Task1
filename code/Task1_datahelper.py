bd69776b08dc32afaa4e4cf2dff578005da24f0c
huuthuc <huuthuc997@gmail.com>
2018-08-19 15:17:51 +0700

    Second commit

--- code/Task1_datahelper.py
+++ code/Task1_datahelper.py
@@ -8,7 +8,7 @@ import numpy as np
 import re
 from convertXLStoXLSX import writeToExcelXLSX
 
-LIMIT_LENGTH_OF_SENTENCES = 100
+LIMIT_LENGTH_OF_SENTENCES = 200
 
 def readFileTSV(dirname='../train_tsv'):
     sentences = []
@@ -19,14 +19,12 @@ def readFileTSV(dirname='../train_tsv'):
     leng = 0
 
     for filename in os.listdir(dirname):
-
         with open(os.path.join(dirname, filename)) as f:
-
             for _, line in enumerate(f.readlines()):
                 row = line.strip()
                 row = re.split("\t", row)
-                if len(row) == 1 or row[1] == '*':
-                    if leng <= LIMIT_LENGTH_OF_SENTENCES:
+                if len(row) == 1:
+                    if leng > 0 and leng <= LIMIT_LENGTH_OF_SENTENCES:
                         sentences.append(sent)
                         labels.append(label)
                         sequence_lengths.append(leng)
@@ -35,103 +33,185 @@ def readFileTSV(dirname='../train_tsv'):
                     sent = []
                     
                 elif len(row) == 3:
-                    sent.append(row[0].lower())
+                    sent.append(row[0].lower().strip())
                     label.append(row[1])
                     leng += 1
-    # for d in set(db):
-    #     print(repr(d))
-    return sentences, labels, sequence_lengths
 
-def convert_word_to_vec(sentences, sequence_lengths, filename='../bio-domain word2vec/PubMed-shuffle-win-30.bin'):
-    vectors = []
-    nrow = len(sentences)
-    ncol = len(sentences[0])
-    for idx in range(nrow):
-        vectors.append([None]*ncol)
-    model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)
-    cnt = 0
-    total = 0
-    for idx in range(nrow):
+    return np.array(sentences), np.array(labels), np.array(sequence_lengths)
+
+def get_vocabs(sentences, sequence_lengths, max_doc_len):
+    vocabs = []
+    n_samples = len(sentences)
+    for idx in range(n_samples):
         sentence = sentences[idx]
-        for subidx in range(ncol):
-            if subidx < sequence_lengths[idx]:
-                total += 1
-            if sentence[subidx] in model and subidx < sequence_lengths[idx]:
-                cnt += 1
-                vectors[idx][subidx] = model[sentence[subidx]]
-    print("Number of words in word2vec: %d in total %d words" % (cnt, total))
+        for subidx in range(min(max_doc_len, sequence_lengths[idx])):
+            if sentence[subidx] not in vocabs:
+                vocabs.append(sentence[subidx])
+    vocabs.append(' ')
+    return vocabs
+
+def generate_char_dict():
+    char_dict = {}
+    alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:’"/|_#$%ˆ&*˜‘+=<>()[]{} '
+    for i,c in enumerate(alphabet):
+        char_dict[c] = i+1
+    return char_dict
+
+def char2vec(word, max_word_len, char_dict):
+    data = np.zeros(max_word_len)
+    for i in range(0, len(word)):
+        if i >= max_word_len:
+            break
+        elif word[i] in char_dict:
+            data[i] = char_dict[word[i]]
+        else:
+            # unknown character set to be 68
+            data[i] = len(char_dict.keys())
+    return data
+
+def generate_lookup_word_embedding(vocabs, filename='../bio-domain word2vec/PubMed-shuffle-win-30.bin'):
+    pretrained_word2vec = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)
+    dims = len(pretrained_word2vec[[*pretrained_word2vec.vocab.keys()][0]])
+    n_vocabs = len(vocabs)
+
+    lookup_table = np.zeros(shape=[n_vocabs, dims])
+    index_of_word_in_lookup_table = dict()
+
+    for idx, word in enumerate(vocabs):
+        assert word not in index_of_word_in_lookup_table
+        index_of_word_in_lookup_table[word] = int(idx)
+        if word in pretrained_word2vec:
+            lookup_table[idx, :] = pretrained_word2vec[word]
+        else:
+            lookup_table[idx, :] = np.random.randn(dims)
+    return lookup_table, index_of_word_in_lookup_table
+
+def encode_sentences(sentences, max_doc_len, lookup_table, index_of_word_in_lookup_table):
+    encoded_sentences = np.zeros(shape=[sentences.shape[0], max_doc_len], dtype=np.int32)
+    for idx_sent, sent in enumerate(sentences):
+        for idx_word_in_sent in range(max_doc_len):
+            word = sent[idx_word_in_sent]
+            idx_of_word = index_of_word_in_lookup_table[word]
+            encoded_sentences[idx_sent,idx_word_in_sent] = idx_of_word
+    return encoded_sentences
     
-    dims = len(model[[*model.vocab.keys()][0]])
-    del model
-
-    random_word2vec = dict()
-    for idx in range(nrow):
-        sentence = sentences[idx]
-        for subidx in range(ncol):
-            if vectors[idx][subidx] is None:
-                if sentences[idx][subidx] not in random_word2vec:
-                    random_word2vec[sentences[idx][subidx]] = np.random.randn(dims)
-                vectors[idx][subidx] = random_word2vec[sentences[idx][subidx]]
-
-    return vectors
-
-def train_dev_split(sentences, vectors, labels, sequence_lengths):
-    n_samples = len(vectors)
-    n_train_samples = int(0.9*n_samples)
+def train_dev_split(sentences, labels, sequence_lengths, train_ratio=0.8):
+    n_samples = sentences.shape[0]
+    n_train_samples = int(train_ratio*n_samples)
     
-    train_vectors, dev_vectors = vectors[:n_train_samples], vectors[n_train_samples:]
-    train_labels, dev_labels = labels[:n_train_samples], labels[n_train_samples:]
-    train_sequence_lengths, dev_sequence_lengths = sequence_lengths[:n_train_samples], sequence_lengths[n_train_samples:]
-    train_sent, dev_sent = sentences[:n_train_samples], sentences[n_train_samples:]
+    train_idx = np.random.choice(n_samples, n_train_samples, replace=False)
+    test_idx = np.array(list(set(range(n_samples)) - set(train_idx)))
+
+    train_sent, dev_sent = sentences[train_idx], sentences[test_idx]
+    train_labels, dev_labels = labels[train_idx], labels[test_idx]
+    train_sequence_lengths, dev_sequence_lengths = sequence_lengths[train_idx], sequence_lengths[test_idx]
+    
     return {
         "train_sentences": train_sent,
         "dev_sentences": dev_sent,
-        "train_vectors": train_vectors,
-        "dev_vectors": dev_vectors,
         "train_labels": train_labels,
         "dev_labels": dev_labels,
         "train_sequence_lengths": train_sequence_lengths,
         "dev_sequence_lengths": dev_sequence_lengths
     }
 
-def labels_sequence_to_int(labels):
+def get_labels_template(labels):
     labels_template = []
     for label in labels:
         for sublabel in label:
             if sublabel not in labels_template:
                 labels_template.append(sublabel)
-    labels_int = []
-    for label in labels:
-        label_int = []
+    return labels_template
+
+def encode_labels(labels, max_doc_len, labels_template):
+    labels_encoded = np.zeros(shape=[labels.shape[0], max_doc_len], dtype=np.int32)
+    for idx, label in enumerate(labels):
+        for subidx in range(max_doc_len):
+            sublabel = label[subidx]
+            labels_encoded[idx, subidx] = labels_template.index(sublabel)
+    return labels_encoded
+
+def decode_labels(encoded_labels, labels_template):
+    labels_decoded = []
+    for label in encoded_labels:
+        label_decoded = []
         for sublabel in label:
-            label_int.append(labels_template.index(sublabel))
-        labels_int.append(label_int)
-    return labels_int, labels_template
-def add_padding(sentences, labels, sequence_lengths, dims=200):
+            label_decoded.append(labels_template[sublabel])
+        labels_decoded.append(label_decoded)
+    return np.array(labels_decoded)
+
+def get_max_doc_len(sequence_lengths):
     max_doc_len = 0
     for leng in sequence_lengths:
         max_doc_len = max(max_doc_len, leng)
+    return max_doc_len
+def add_padding(sentences, labels, sequence_lengths, max_doc_len, dims=200):
     for idx, leng in enumerate(sequence_lengths):
         gap = LIMIT_LENGTH_OF_SENTENCES - leng
         sentences[idx].extend([' ']*gap)
         labels[idx].extend(['PAD']*gap)
-    return max_doc_len
+    
 
+def batch_iter(sentences, labels, sequence_lengths, batch_size=32, num_epochs=1000, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    n_samples = sentences.shape[0]
+    num_batches_per_epoch = int((n_samples-1)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(n_samples))
+
+            sentences = sentences[shuffle_indices]
+            labels = labels[shuffle_indices]
+            sequence_lengths = sequence_lengths[shuffle_indices]
+        
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, n_samples)
+            yield sentences[start_index:end_index], labels[start_index:end_index], sequence_lengths[start_index:end_index]
+
+def get_word_from_idx(index_of_word_in_lookup_table, idx):
+    for word in index_of_word_in_lookup_table:
+        if index_of_word_in_lookup_table[word] == int(idx):
+            return word
+    return ' '
 def preprocess():
     sentences, labels, sequence_lengths = readFileTSV()
-    max_doc_len = add_padding(sentences, labels, sequence_lengths)
-    vectors = convert_word_to_vec(sentences, sequence_lengths)
-    
-    
-    labels_int, labels_template = labels_sequence_to_int(labels)
+    max_doc_len = get_max_doc_len(sequence_lengths)
+    add_padding(sentences, labels, sequence_lengths, max_doc_len)
 
-    data = train_dev_split(sentences, vectors, labels_int, sequence_lengths)
+    #lấy toàn bộ label và unique, đưa vào labels_template
+    labels_template = get_labels_template(labels)
+    encoded_labels = encode_labels(labels, max_doc_len, labels_template) #chứa labels dạng số #cần tối ưu
+
+    vocabs = get_vocabs(sentences, sequence_lengths, max_doc_len)
+    lookup_table, index_of_word_in_lookup_table = generate_lookup_word_embedding(vocabs)
+    encoded_sentences = encode_sentences(sentences, max_doc_len, lookup_table, index_of_word_in_lookup_table) #chứa sentence dạng số
+
+    data = train_dev_split(encoded_sentences, encoded_labels, sequence_lengths, train_ratio=0.8)
     data['labels_template'] = labels_template
     data['max_doc_len'] = max_doc_len
+    data['word_embedding_lookup_table'] = lookup_table
+    data['index_of_word_in_lookup_table'] = index_of_word_in_lookup_table
     return data    
 
+
 if __name__ == '__main__':
-    sents, _, _ = readFileTSV()
-    print('Total length:', len(sents))
-    
+    sentences, labels, sequence_lengths = readFileTSV()
+    max_doc_len = get_max_doc_len(sequence_lengths)
+    add_padding(sentences, labels, sequence_lengths, max_doc_len)
+
+    #lấy toàn bộ label và unique, đưa vào labels_template
+    labels_template = get_labels_template(labels)
+    encoded_labels = encode_labels(labels, max_doc_len, labels_template) #chứa labels dạng số
+
+    vocabs = get_vocabs(sentences, sequence_lengths, max_doc_len)
+    lookup_table, index_of_word_in_lookup_table = generate_lookup_word_embedding(vocabs)
+    encoded_sentences = encode_sentences(sentences, max_doc_len, lookup_table, index_of_word_in_lookup_table) #chứa sentence dạng số
+
+    print("sents shape: ", encoded_sentences.shape)
+    print("labels shape: ", encoded_labels.shape)
+    print("lengths shape: ", sequence_lengths.shape)
     
--- code/Task1_model_and_train.py
+++ code/Task1_model_and_train.py
@@ -4,33 +4,23 @@ import numpy as np
 import tensorflow as tf
 import time
 import csv
-from Task1_datahelper import preprocess
-
+from Task1_datahelper import *
 
 data = preprocess()
+
 max_doc_len = data['max_doc_len']
 labels_template = data['labels_template']
-train_sent = data["train_sentences"]
 
-train_vectors = data["train_vectors"]
-
+train_sentences = data["train_sentences"]
 train_labels = data["train_labels"]
-
 train_sequence_lengths = data["train_sequence_lengths"]
 
-
-dev_sent = data["dev_sentences"]
-dev_vectors = data["dev_vectors"]
+dev_sents = data["dev_sentences"]
 dev_labels = data["dev_labels"]
 dev_sequence_lengths = data["dev_sequence_lengths"]
 
-
-labels_template = np.array(labels_template)
-train_sent = np.array(train_sent)
-train_vectors = np.array(train_vectors)
-train_labels = np.array(train_labels)
-train_sequence_lengths = np.array(train_sequence_lengths)
-
+word_lookup_table = data['word_embedding_lookup_table']
+index_of_word_in_lookup_table = data['index_of_word_in_lookup_table']
 
 embedding_size = 200
 num_classes = len(labels_template)
@@ -38,30 +28,25 @@ hidden_size_lstm = 120
 dropout_prob = 0.5
 n_epochs = 1
 batch_size = 32
-n_batches = int(train_vectors.shape[0]//batch_size) + 1
+n_batches = int(train_sentences.shape[0]//batch_size) + 1
 learning_rate = 0.001
 momentum = 0.9
 
-vectors_placeholder = tf.placeholder(tf.float32, shape=[None, max_doc_len, embedding_size])
-labels_placeholder = tf.placeholder(tf.int32, shape=[None,max_doc_len])
-sequence_lengths_placeholder = tf.placeholder(tf.int32, shape=[None])
 
-train_dataset = tf.data.Dataset \
-                .from_tensor_slices((vectors_placeholder, labels_placeholder, sequence_lengths_placeholder)) \
-                .shuffle(buffer_size=2).batch(batch_size).repeat()
+sentences_placeholder = tf.placeholder(tf.int32, shape=[None, max_doc_len], name='sentences')
+labels_placeholder = tf.placeholder(tf.int32, shape=[None,max_doc_len], name='labels')
+sequence_lengths_placeholder = tf.placeholder(tf.int32, shape=[None], name='lengths')
 
-
-train_iterator = train_dataset.make_initializable_iterator()
-
-
-x, y, sequence_length = train_iterator.get_next()
+with tf.variable_scope('word-embedding-layer'):
+    W_embedding = tf.Variable(initial_value=word_lookup_table, dtype=tf.float32, trainable=False, name='word-embedding')
+    vectors = tf.nn.embedding_lookup(W_embedding, sentences_placeholder)
 
 with tf.variable_scope("bi-lstm"):
     cell_fw = tf.contrib.rnn.LSTMCell(hidden_size_lstm)
     cell_bw = tf.contrib.rnn.LSTMCell(hidden_size_lstm)
     (output_fw, output_bw), _ = tf.nn.bidirectional_dynamic_rnn( \
-                                cell_fw, cell_bw, x, \
-                                sequence_length=sequence_length, dtype=tf.float32)
+                                cell_fw, cell_bw, vectors, \
+                                sequence_length=sequence_lengths_placeholder, dtype=tf.float32)
     output = tf.concat([output_fw, output_bw], axis=-1)
     output = tf.nn.dropout(output, dropout_prob)
 
@@ -77,77 +62,58 @@ with tf.variable_scope("projection"):
 
 with tf.name_scope('crf_encode'):
 
-    log_likelihood, trans_params = tf.contrib.crf.crf_log_likelihood(logits, y, sequence_length)
+    log_likelihood, trans_params = tf.contrib.crf.crf_log_likelihood(logits, labels_placeholder, sequence_lengths_placeholder)
     loss = tf.reduce_mean(-log_likelihood)
 with tf.name_scope('crf_decode'):
-    viterbi_sequence, viterbi_score = tf.contrib.crf.crf_decode(logits, trans_params, sequence_length)
+    viterbi_sequence, viterbi_score = tf.contrib.crf.crf_decode(logits, trans_params, sequence_lengths_placeholder)
 
 optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=momentum).minimize(loss)
 
 
 with tf.Session() as sess:
     sess.run( tf.global_variables_initializer())
-    sess.run(train_iterator.initializer, feed_dict={vectors_placeholder:train_vectors,
-                                            labels_placeholder:train_labels,
-                                            sequence_lengths_placeholder:train_sequence_lengths})
-    step = 0
     print("Training: Start")
-    for epoch in range(n_epochs):
-        for batch in range(n_batches):
-            loss_, _, _, _, _ = sess.run([loss, optimizer, x, y, sequence_length])
-            step += 1
-            if step % 10 == 0:
-                print("Epoch %d/%d  Batch %d/%d Loss: %f" % (epoch, n_epochs - 1, batch, n_batches - 1, loss_))
-        print()
 
-    del train_sent, train_vectors, train_labels, train_sequence_lengths
+    step = 0
+    batches = batch_iter(train_sentences, train_labels, train_sequence_lengths, batch_size=batch_size, num_epochs=n_epochs, shuffle=True)
+    for batch in batches: 
+        sent_batch, label_batch, sequence_length_batch = batch
+
+        loss_, _, predict = sess.run([loss, optimizer, viterbi_sequence], feed_dict={
+                                                                                sentences_placeholder: sent_batch, 
+                                                                                labels_placeholder: label_batch, 
+                                                                                sequence_lengths_placeholder: sequence_length_batch
+                                                                            })
+        step += 1
+        if step % 100 == 0:
+            print("Step %d/%d Loss: %f" % (step, n_batches*n_epochs, loss_))
+            
+        
+    print()
+
+    del train_sentences, train_labels, train_sequence_lengths
     print("Training: Done")
     
     print("\n\n\n")
     
     print('Developing: Start')
-    ids = np.array(list(range(len(dev_sent))))
-    idx_placeholder = tf.placeholder(tf.int32, shape = [None])
-    dev_dataset = tf.data.Dataset \
-                .from_tensor_slices((idx_placeholder, vectors_placeholder, labels_placeholder, sequence_lengths_placeholder)).batch(1)
-    dev_iterator = dev_dataset.make_initializable_iterator()
-    
+    tsvfile = open('../eval_dev/predict_file.tsv', 'w')
+    for idx in range(dev_sents.shape[0]):
+        sent = dev_sents[idx].reshape(1, -1)
+        label = dev_labels[idx].reshape(1, -1)
+        sequence_length = np.array([dev_sequence_lengths[idx]])
 
-    dev_sent = np.array(dev_sent)
-    dev_vectors = np.array(dev_vectors)
-    dev_labels = np.array(dev_labels)
-    dev_sequence_lengths = np.array(dev_sequence_lengths)
-    predict_tags = []
-    dev_idx, dev_x, dev_y, dev_sequence_length = dev_iterator.get_next()
-    sess.run(dev_iterator.initializer, feed_dict={
-                                            idx_placeholder: ids,
-                                            vectors_placeholder:dev_vectors,
-                                            labels_placeholder:dev_labels,
-                                            sequence_lengths_placeholder:dev_sequence_lengths})
-
-    f = open('predict_file.tsv', 'w')
-    tsvfile = csv.writer(f, delimiter=' ')
+        predict = sess.run(viterbi_sequence, feed_dict={sentences_placeholder: sent,
+                                                        labels_placeholder: label,
+                                                        sequence_lengths_placeholder: sequence_length})
+        sent = sent[0]
+        label = label[0]
+        if sequence_length[0] > 0:
+            for subidx in range(sequence_length[0]):        
+                word = get_word_from_idx(index_of_word_in_lookup_table, sent[subidx])
+                golden_tag = labels_template[label[subidx]]
+                predict_tag = labels_template[predict[0][subidx]]
+                tsvfile.write("%s\t%s\t%s\n" % (word, golden_tag, predict_tag))
+            tsvfile.write('-\tX\t-\n')
         
-    for _ in range(dev_vectors.shape[0]):
-        predict = sess.run(viterbi_sequence)
-        idx_sent, _, ground_labels, seq_len = sess.run([dev_idx, dev_x, dev_y, dev_sequence_length])
-        print(predict.shape)
-        
-
-        for subidx in range(len(seq_len)):
-            word = dev_sent[idx_sent,subidx]
-            golden_tag = labels_template[ground_labels[subidx]]
-                
-            predict_tag = labels_template[predict[subidx]]
-            tsvfile.writerow([word, golden_tag, predict_tag])
-        tsvfile.writerow('-X-')
-        
-    f.close()
     print("Developing: Done")
-
-    print("\n")
-
-
-    print("Writing predict to file")
-
-    print("Done")
